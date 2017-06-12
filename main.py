98366f7292afc1619e2a8abe8aea1e9dd7e53694
reishin <hapy.lestat@gmail.com>
2017-06-12 14:11:32 +0300

    add only networks

--- main.py
+++ main.py
@@ -10,9 +10,11 @@ prefixes_ipv4 = []
 prefixes_ipv6 = []
 
 as_list = []
+nets_list = []
 
 for net in nets.items:
     as_list.extend(net.as_list)
+    nets_list.extend(net.nets)
 
 nets = curl("https://stat.ripe.net/data/bgp-state/data.json?resource={}".format(",".join(as_list))).from_json()
 
--- models/__init__.py
+++ models/__init__.py
@@ -5,6 +5,7 @@ from apputils.types.config import ConfigObject
 class NetworkItem(ConfigObject):
     name = None
     as_list = []
+    nets = []
 
 
 class Networks(ConfigObject):
