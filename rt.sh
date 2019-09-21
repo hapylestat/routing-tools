2e62407d0bdba4e29add8611f4b57bffeddbc5fc
reishin <hapy.lestat@gmail.com>
2019-09-21 17:29:00 +0300

    Add profiles support for several vpn endpoints

--- rt.sh
+++ rt.sh
@@ -1,61 +1,64 @@
 #!/bin/bash
 
 MYDIR=$(dirname ${BASH_SOURCE[0]})
-APP=uablock  # used for ipset table name
-MYTABLE=160
-MYFWMARK=0xaf
+
+if [ -z $2 ]; then
+ echo "Please set profile name to use"
+ exit 1
+fi
+
+. $MYDIR/conf/profiles/$2
+
+APP=${APP:-}  # used for ipset table name
+MYTABLE=${MYTABLE:-}
+MYFWMARK=${MYFWMARK:-}
+DEV=${DEV:-}
+IPROUTE=${IPROUTE:-}
 
 
 do_custom_route(){
- local IF_DEV=$1
- local ifIP=$2
- local rules=$3
+ local rules=$1
 
- ping -c 1 -n -W 1 ${ifIP} 1>/dev/null 2>&1
+ ping -c 1 -n -W 1 ${IPROUTE} 1>/dev/null 2>&1
  if [ $? -ne 0 ]; then
    echo "Router ping test failed...aborting"
    exit 1
  fi
 
- echo "My Router IP: ${ifIP}"
+ echo "My Router IP: ${IPROUTE}/${DEV}"
 
  ipset create ${APP} hash:net 1>/dev/null 2>&1
- if [ $? -eq 0 ]; then
-  echo "Getting sub-nets list for ${rules}..."
-  local ROUTES=`python ${MYDIR}/main.py --nets=${rules}`
 
-  echo  "Publishing networks to storage..."
-  for r in ${ROUTES}; do
-    ipset add ${APP} ${r}
-  done
- else
-  echo "Using cached routes (run rt.sh reset to update cache) ..".
- fi
+ echo "Getting sub-nets list for ${rules}..."
+ local ROUTES=`python ${MYDIR}/main.py --nets="${rules}"`
+
+ echo  "Publishing networks to storage..."
+ for r in ${ROUTES}; do
+  ipset add ${APP} ${r}
+ done
 
  iptables -t mangle -A PREROUTING -m set --match-set ${APP} dst -j MARK --set-mark ${MYFWMARK}
  iptables -t mangle -A OUTPUT -m set --match-set ${APP} dst -j MARK --set-mark ${MYFWMARK}
 
  ip rule add fwmark ${MYFWMARK} table ${MYTABLE}
- ip route add default via ${ifIP} table ${MYTABLE} prio 100
+ ip route add default via ${IPROUTE} dev ${DEV} table ${MYTABLE} prio 100
 
 
 
  # allow async routes to work properly
- echo 2 >  /proc/sys/net/ipv4/conf/${IF_DEV}/rp_filter
+ echo 2 >  /proc/sys/net/ipv4/conf/${DEV}/rp_filter
 }
 
 
 do_route(){
- local IF_DEV=$1
- local ifIP=$2
  
- ping -c 1 -n -W 1 ${ifIP} 1>/dev/null 2>&1
+ ping -c 1 -n -W 1 ${IPROUTE} 1>/dev/null 2>&1
  if [ $? -ne 0 ]; then
    echo "Router ping test failed...aborting"
    exit 1
  fi
  
- echo "My Router IP: ${ifIP}" 
+ echo "My Router IP: ${IPROUTE}" 
 
  ipset create ${APP} hash:net 1>/dev/null 2>&1
  if [ $? -eq 0 ]; then 
@@ -74,21 +77,19 @@ do_route(){
  iptables -t mangle -A OUTPUT -m set --match-set ${APP} dst -j MARK --set-mark ${MYFWMARK}
  
  ip rule add fwmark ${MYFWMARK} table ${MYTABLE}
- ip route add default via ${ifIP} table ${MYTABLE} prio 100
+ ip route add default via ${IPROUTE} table ${MYTABLE} prio 100
  
  
  
  # allow async routes to work properly 
- echo 2 >  /proc/sys/net/ipv4/conf/${IF_DEV}/rp_filter
+ echo 2 >  /proc/sys/net/ipv4/conf/${DEV}/rp_filter
  
 }
 
 do_unroute(){
  echo "Removing routes..."
- local IF_DEV=$1
- local ifIP=$2
  
- ping -c 1 -n -W 1 ${ifIP} 1>/dev/null 2>&1
+ ping -c 1 -n -W 1 ${IPROUTE} 1>/dev/null 2>&1
  if [ $? -ne 0 ]; then
    echo "Router ping test failed...aborting"
    exit 1
@@ -98,7 +99,7 @@ do_unroute(){
  iptables -t mangle -D OUTPUT -m set --match-set ${APP} dst -j MARK --set-mark ${MYFWMARK} 1>/dev/null 2>&1
 
  ip rule del fwmark ${MYFWMARK} table ${MYTABLE} 1>/dev/null 2>&1
- ip route del prio 100 via ${ifIP} table ${MYTABLE} 1>/dev/null 2>&1
+ ip route del prio 100 via ${IPROUTE} table ${MYTABLE} 1>/dev/null 2>&1
  ip route del default table ${MYTABLE} prio 100
 }
 
@@ -111,39 +112,24 @@ do_reset_cache(){
 
 case $1 in
   start)
-   if [ -z $2 ] || [ -z $3 ]; then
-     echo "Please provide interface name and routing gw ip(remote point) as argument"
-     exit 1
-   fi
-   
-   do_route $2 $3
+   do_route
   ;;
-  part-start)
-   if [ -z $2 ] || [ -z $3 ] || [ -z $4 ]; then
-     echo "Please provide interface name,  routing gw ip(remote point) and rule list to apply as argument"
+  update)
+   if [ -z $3 ]; then
+     echo "please provide rule list to apply as argument"
      exit 1
    fi
 
-   do_custom_route $2 $3 "$4"
+   do_custom_route "$3"
   ;;
   stop)
-   if [ -z $2 ] || [ -z $3 ]; then
-     echo "Please provide interface name and routing gw ip(remote point) as argument"
-     exit 1
-   fi
-
-   do_unroute $2 $3
+   do_unroute
   ;;
   reset)
-   if [ -z $2 ] || [ -z $3 ]; then
-     echo "Please provide interface name and routing gw ip(remote point) as argument"
-     exit 1
-   fi
-
-   do_unroute $2 $3
+   do_unroute
    do_reset_cache
   ;;
   *)
-  echo "rt.sh start|part-start|stop|reset  <Route if device name> <Route GW ip addr> [rule name for part-start]"
+  echo "rt.sh start|update|stop|reset  <profile name> [rule name]"
   ;;
 esac
